9990d24dfea016ef97e545ac666bfc72d41fb787
Italo <italoc.alb@gmail.com>
2022-04-19 18:19:35 -0300

    updating the main function to test the framework

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+from core.renderer import Renderer
+from core.scene import Scene
+from core.camera import Camera
+from core.mesh import Mesh
+from geometry.geometry import *
+from material.material import *
+import pyglet
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    config = pyglet.gl.Config(double_buffer=True)
+
+    win = pyglet.window.Window(config=config)
+
+    renderer = Renderer()
+    scene = Scene()
+    camera = Camera()
+
+    geometry = RectangleGeometry()
+    material = SurfaceMaterial()
+
+    mesh = Mesh(geometry, material)
+
+    scene.add(mesh)
+
+    renderer.render(scene, camera)
+
+    pyglet.app.run()
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
