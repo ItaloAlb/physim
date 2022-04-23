921aa6818d4bcb721c0e9a176dcedf7baafa7c08
Italo <italoc.alb@gmail.com>
2022-04-23 08:00:54 -0300

    developing skeleton code

--- physics/collisionhandler.py
+++ physics/collisionhandler.py
@@ -1,6 +1,10 @@
 from physics.particlesystem import ParticleSystem
 
 
+# Collision Handler is a graphical concept to solve collision problems.
 class CollisionHandler:
-    def __init__(self, particleSystem: ParticleSystem):
+    def __init__(self):
         pass
+
+    def __call__(self, pos, nextPos, **kwargs):
+        return nextPos
--- physics/particlesystem.py
+++ physics/particlesystem.py
@@ -1,18 +1,39 @@
 from geometry.geometry import *
 from material.material import *
 from core.mesh import *
+from physics.collisionhandler import *
 
+
+# Particle System is a physical object that evolves in time.
+# It will store information about all particles.
+# Also, it will call a collision handler to solve collision problems.
 class ParticleSystem:
     def __init__(self):
+
+        # array list with all information about particles
+        # particles[i][j]: i = [0, 1, 2] or [mass, position, velocity] ; j = [0, ..., n] each particle
         self.particles = []
 
+        self.collisionHandler = CollisionHandler()
+
         # setup graphics for bounding box
-        self._geometry = BoundingBoxGeometry()
-        self._material = LineMaterial()
-        self._mesh = Mesh(self._geometry, self._material)
+        self._boundBoxGeo = BoundingBoxGeometry()
+        self._boundBoxMat = LineMaterial()
+        self._boundBoxMesh = Mesh(self._boundBoxGeo, self._boundBoxMat)
+
+        # setup graphics for particles
+        self._particleGeo = PointGeometry()
+        self._particleMat = PointMaterial()
+        self._particleMesh = Mesh(self._particleGeo, self._particleMat)
+
+
 
     def add_particle(self, particle) -> None:
         self.particles.append(particle)
 
     def remove_particle(self, particle) -> None:
         self.particles.remove(particle)
+
+    def update(self, dt: float):
+        # updating all particles position by calling collision handler
+        self.particles[1] = self.collisionHandler(self.particles[1], self.particles[1] + self.particles[2] * dt)
