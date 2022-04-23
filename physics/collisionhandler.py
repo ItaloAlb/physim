98633916ccfce412e3d3171ffe7603fa5791491f
Italo <italoc.alb@gmail.com>
2022-04-23 11:54:40 -0300

    feat: boundary collision

--- physics/collisionhandler.py
+++ physics/collisionhandler.py
@@ -1,10 +1,35 @@
-from physics.particlesystem import ParticleSystem
+from typing import Tuple
+
+import numpy
+# from physics.particlesystem import ParticleSystem
 
 
 # Collision Handler is a graphical concept to solve collision problems.
+from numpy import ndarray
+
+
 class CollisionHandler:
     def __init__(self):
         pass
 
-    def __call__(self, pos, nextPos, **kwargs):
-        return nextPos
+    def __call__(self, **kwargs):
+        pPos = kwargs.get("pPos")
+        pNextPos = kwargs.get("pNextPos")
+        pVel = kwargs.get("pVel")
+        boundBox = kwargs.get("boundBox", 1)
+
+        # check and response: particle-to-bounding collision
+
+        # create a list with all collision index where pNextPos is out of bound
+        colCheck = numpy.where(pNextPos > boundBox / 2, True, False)
+        pVel = numpy.where(colCheck, -pVel, pVel)
+        colCheck = numpy.where(pNextPos < - boundBox / 2, True, False)
+        pVel = numpy.where(colCheck, -pVel, pVel)
+
+        pNextPos = numpy.where(pNextPos > boundBox / 2, 2 * boundBox / 2 - pNextPos, pNextPos)
+        pNextPos = numpy.where(pNextPos < - boundBox / 2, 2 * (- boundBox / 2) - pNextPos, pNextPos)
+
+
+        # check and response: particle-to-particle collision
+
+        return pNextPos, pVel
--- physics/particlesystem.py
+++ physics/particlesystem.py
@@ -1,3 +1,5 @@
+import numpy
+
 from geometry.geometry import *
 from material.material import *
 from core.mesh import *
@@ -10,30 +12,48 @@ from physics.collisionhandler import *
 class ParticleSystem:
     def __init__(self):
 
-        # array list with all information about particles
-        # particles[i][j]: i = [0, 1, 2] or [mass, position, velocity] ; j = [0, ..., n] each particle
-        self.particles = []
+        # numpy array with all information about particles
+        # each value is assigned to one particle
+        self.pMass = numpy.asarray([1])
+        # every three values is assigned to one particle.
+        # ex: pPos[0], pPos[1], pPos[2] -> first particle position
+        # ex: pPos[3], pPos[4], pPos[5] -> second particle position
+        self.pPos = numpy.asarray([0, 0, -5])
+        # same as particle position array.
+        # every three value is assigned to one particle.
+        self.pVel = numpy.asarray([0, 0, 0])
 
+        # instance of Collision Handler class
         self.collisionHandler = CollisionHandler()
 
         # setup graphics for bounding box
         self._boundBoxGeo = BoundingBoxGeometry()
         self._boundBoxMat = LineMaterial()
-        self._boundBoxMesh = Mesh(self._boundBoxGeo, self._boundBoxMat)
+        self.boundBoxMesh = Mesh(self._boundBoxGeo, self._boundBoxMat)
 
         # setup graphics for particles
-        self._particleGeo = PointGeometry()
+        self._particleGeo = ParticleSystemGeometry(self.pPos)
         self._particleMat = PointMaterial()
-        self._particleMesh = Mesh(self._particleGeo, self._particleMat)
+        self.particleMesh = Mesh(self._particleGeo, self._particleMat)
 
+    def add_particle(self, mass, position, velocity) -> None:
+        numpy.append(self.pMass, mass)
+        numpy.append(self.pPos, position)
+        numpy.append(self.pVel, velocity)
 
+    def remove_particle(self, i: int) -> None:
+        numpy.delete(self.pMass, i)
+        numpy.delete(self.pPos, i)
+        numpy.delete(self.pVel, i)
 
-    def add_particle(self, particle) -> None:
-        self.particles.append(particle)
-
-    def remove_particle(self, particle) -> None:
-        self.particles.remove(particle)
-
-    def update(self, dt: float):
+    def update(self, dt: float = 1 / 60):
         # updating all particles position by calling collision handler
-        self.particles[1] = self.collisionHandler(self.particles[1], self.particles[1] + self.particles[2] * dt)
+        # self.pPos = self.pPos + self.pVel * dt
+        self.pPos, self.pVel = self.collisionHandler(pPos = self.pPos, pNextPos = self.pPos + self.pVel * dt, pVel = self.pVel)
+
+        attrib = self._particleGeo.attrib["vertexPosition"]
+        attrib.attribData = self.pPos
+        attrib.uploadData()
+
+    def __call__(self):
+        self.update()
