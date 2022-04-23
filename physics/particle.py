fd059f7ebfee4b13f5ab4996b5da2a4fd50131f3
Italo <italoc.alb@gmail.com>
2022-04-23 07:59:39 -0300

    att: particle class obsolete

--- physics/particle.py
+++ physics/particle.py
@@ -4,10 +4,16 @@ from core.mesh import *
 import numpy
 
 
+# Particle is a conceptual physical object used to simplify and to model natural phenomenon, like celestial.
+# A particle is used to describe very small objects. Very small is determined based upon your model.
+# Also, a particle have physical properties such as mass, position, velocity and so on and so forth.
+# This class will store all information about a single particle themselves.
+
+### OBSOLETE ###
 class Particle:
-    def __init__(self, mass: float, position: numpy.ndarray, velocity: numpy.ndarray, acceleration: numpy.ndarray):
+    def __init__(self, mass: float, position: numpy.ndarray, velocity: numpy.ndarray):
         # initialize all particles physics properties
-        self.mass, self.position, self.velocity, self.acceleration = mass, position, velocity, acceleration
+        self.mass, self.position, self.velocity = mass, position, velocity
 
         # setup graphics for particle
         self._geometry = PointGeometry()
