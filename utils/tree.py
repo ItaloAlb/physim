abc4cddd28d03dcfefb2500643aac8bdfaeb6dae
Italo <italoc.alb@gmail.com>
2022-04-30 21:23:21 -0300

    feat: node's data now is an instance of ptr class update: bug fixing at parent's, children's, data's get and setters. update 2: major updating at Node's parameters

--- utils/tree.py
+++ utils/tree.py
@@ -1,22 +1,31 @@
+from utils.pointer import *
+
 
 # Abstract base class representing a tree structure
 class Tree:
 
     # Abstract node class
     class Node:
-        def __init__(self, parent = None, children = None, data = None):
-            self.parent = parent
-            self.children = children
-            self.data = data
 
-        # Return the data stored at this node
-        @property
-        def data(self):
-            return self._data
+        data = ptr()
 
-        @data.setter
-        def data(self, d):
-            self._data = d
+        def __init__(self, **kwargs):
+            self.parent, self.data, self.children, self.tag = kwargs.get('parent', None), \
+                                                              kwargs.get('data', None), \
+                                                              kwargs.get('children', None), \
+                                                              kwargs.get('tag', None)
+
+
+        # # Return the data stored at this node
+        # @property
+        # def data(self):
+        #     return self._data
+        #
+        # @data.setter
+        # def data(self, d):
+        #     if not(isinstance(d, list) or d is None):
+        #         raise TypeError('data must be a list')
+        #     self._data = [] if d is None else d
 
         # Return Node representing node's parent (or None if node is root)
         @property
@@ -25,7 +34,9 @@ class Tree:
 
         @parent.setter
         def parent(self, p):
-            self._parent = p if isinstance(p, self.__class__) or p is None else TypeError('parent must be a node')
+            if not(isinstance(p, self.__class__) or p is None):
+                raise TypeError('parent must be a node')
+            self._parent = p
 
         # Generate an iteration of Node representing node's children
         @property
@@ -34,17 +45,21 @@ class Tree:
 
         @children.setter
         def children(self, c):
-            self._children = [] if c is None else (c if isinstance(c, list) else TypeError('children must be a list'))
+            if not(isinstance(c, list) or c is None):
+                raise TypeError('children must be a list')
+            self._children = [] if c is None else c
 
         # Return the number of children that Node node has
         def num_children(self):
             return len(self._children)
 
         # Return True if Node node represents the root of the tree
+        @property
         def is_root(self):
             return self.parent is None
 
         # Return True if Node node doesn't have any children
+        @property
         def is_leaf(self):
             return self.num_children() == 0
 
@@ -56,8 +71,11 @@ class Tree:
         def __ne__(self, other):
             return self.data != other.data
 
-    def __init__(self):
-        self.root = self.Node()
+    def __init__(self, **kwargs):
+        self.root = Tree.Node(
+            data=kwargs.get('data', None),
+            tag=kwargs.get('tag', 'root')
+        )
 
     # Return root Node of this tree
     @property
@@ -66,12 +84,14 @@ class Tree:
 
     @root.setter
     def root(self, r):
-        self._root = r if isinstance(r, self.Node) else TypeError('root must be a node')
+        if not isinstance(r, self.Node):
+            raise TypeError('root must be a node')
+        self._root = r
 
-    def add_node(self, node):
-        if node.parent in iter(self):
-            node.parent.children.append(node)
-            return
+    def add_node(self, *nodes):
+        for node in nodes:
+            if node.parent in iter(self):
+                node.parent.children.append(node)
 
     def remove_node(self, node):
         if node in iter(self):
@@ -85,7 +105,7 @@ class Tree:
     def depth(self, node):
         if node.is_root():
             return 0
-        return 1 + self.depth(node.parent())
+        return 1 + self.depth(node.parent)
 
 
     def _height(self, node):
@@ -98,6 +118,15 @@ class Tree:
         if node is None:
             node = self.root
         return self._height(node)
+
+    @property
+    def __leaf__(self):
+        leaf = []
+        for node in iter(self):
+            if node.is_leaf:
+                leaf.append(node)
+        return leaf
+
 
     # Return an iterable object of nodes a tree has
     def __iter__(self):
@@ -114,6 +143,8 @@ class Tree:
 
     # Return the number of nodes a tree has
     def __len__(self):
+        if self.root is None:
+            return 0
         queue, count = [self.root], 0
         while len(queue) > 0:
             node = queue.pop(0)
@@ -121,14 +152,83 @@ class Tree:
             count += 1
         return count
 
-tree = Tree()
-node = Tree.Node(tree.root)
-tree.add_node(node)
+    def __str__(self):
+        _str = ""
+        return _str
 
-for node in iter(tree):
-    print(node.is_root())
 
-print('\n')
+class Octree(Tree):
+    class Space(Tree.Node):
+        def __init__(self, **kwargs):
 
-for node in iter(tree):
-    print(node.is_leaf())
+            self.parent, self.center, self.width, self.data, self.children, self.tag = kwargs.get('parent', None), \
+                                                                                       kwargs.get('center', None), \
+                                                                                       kwargs.get('width', None), \
+                                                                                       kwargs.get('data', None), \
+                                                                                       kwargs.get('children', None), \
+                                                                                       kwargs.get('tag', None)
+
+            super().__init__(
+                parent=self.parent,
+                data=self.data,
+                children=self.children,
+                tag=self.tag
+            )
+
+
+        @property
+        def center(self):
+            return self._center
+
+        @center.setter
+        def center(self, c):
+            self._center = c
+
+        @property
+        def width(self):
+            return self._width
+
+        @width.setter
+        def width(self, w):
+            self._width = w
+
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.root = Octree.Space(
+            center=kwargs.get('center', (0, 0, 0)),
+            width=kwargs.get('width', 1),
+            data=kwargs.get('data', None),
+            tag=kwargs.get('tag', 'root')
+        )
+
+    def subdivide(self):
+        if self.is_empty():
+            raise SubdivideEmptyTreeError
+        for leaf in self.__leaf__:
+            c, w = leaf.center, leaf.width / 2
+            nwf, nef, swf, sef = Octree.Space(parent=leaf, center=(c[0] - w, c[1] + w, c[2] - w), width=w, tag="nwf"), \
+                                 Octree.Space(parent=leaf, center=(c[0] + w, c[1] + w, c[2] - w), width=w, tag="nef"), \
+                                 Octree.Space(parent=leaf, center=(c[0] - w, c[1] - w, c[2] - w), width=w, tag="swf"), \
+                                 Octree.Space(parent=leaf, center=(c[0] + w, c[1] - w, c[2] - w), width=w, tag="sef")
+
+            nwb, neb, swb, seb = Octree.Space(parent=leaf, center=(c[0] - w, c[1] + w, c[2] + w), width=w, tag="nwb"), \
+                                 Octree.Space(parent=leaf, center=(c[0] + w, c[1] + w, c[2] + w), width=w, tag="neb"), \
+                                 Octree.Space(parent=leaf, center=(c[0] - w, c[1] - w, c[2] + w), width=w, tag="swb"), \
+                                 Octree.Space(parent=leaf, center=(c[0] + w, c[1] - w, c[2] + w), width=w, tag="seb")
+
+            self.add_node(nwf, nef, swf, sef, nwb, neb, swb, seb)
+
+    # Create a data reference on root's data to leaf's data based on a space partitioning criteria
+    def relocate(self):
+
+        pass
+
+class SubdivideEmptyTreeError(Exception):
+    """Raised when trying to subdivide an empty tree"""
+    pass
+
+# tree = Tree()
+octree = Octree()
+# octree.subdivide()
+# for node in octree.__leaf__:
+#     print("center: ", node.center, "   \t width: ", node.width, "\t tag: ", node.tag)
